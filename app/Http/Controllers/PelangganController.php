1946602dc45a68759b4ac73d871895aa0d09f665
Idoo123456 <mido24si@mahasiswa.pcr.ac.id>
2025-11-20 08:52:31 +0700

    Menambahkan fitur filter dan search

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Pelanggan;
@@ -10,10 +9,12 @@ class PelangganController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $data['dataPelanggan'] = Pelanggan::all();
-		return view('admin.pelanggan.index',$data);
+        $filterableColumns     = ['gender'];
+        $searchableColumns     = ['first_name', 'last_name', 'email', 'phone'];
+        $data['dataPelanggan'] = Pelanggan::filter($request, $filterableColumns)->search($request, $searchableColumns)->simplePaginate(10);
+        return view('admin.pelanggan.index', $data);
     }
 
     /**
@@ -31,15 +32,15 @@ class PelangganController extends Controller
     {
         // dd($request->all());
         $data['first_name'] = $request->first_name;
-		$data['last_name'] = $request->last_name;
-		$data['birthday'] = $request->birthday;
-		$data['gender'] = $request->gender;
-		$data['email'] = $request->email;
-		$data['phone'] = $request->phone;
+        $data['last_name']  = $request->last_name;
+        $data['birthday']   = $request->birthday;
+        $data['gender']     = $request->gender;
+        $data['email']      = $request->email;
+        $data['phone']      = $request->phone;
 
-		Pelanggan::create($data);
+        Pelanggan::create($data);
 
-		return redirect()->route('pelanggan.index')->with('success','Penambahan Data Berhasil!');
+        return redirect()->route('pelanggan.index')->with('success', 'Penambahan Data Berhasil!');
     }
 
     /**
@@ -56,7 +57,7 @@ class PelangganController extends Controller
     public function edit(string $id)
     {
         $data['dataPelanggan'] = Pelanggan::findOrFail($id);
-    return view('admin.pelanggan.edit', $data);
+        return view('admin.pelanggan.edit', $data);
     }
 
     /**
@@ -65,14 +66,14 @@ class PelangganController extends Controller
     public function update(Request $request, string $id)
     {
         $pelanggan_id = $id;
-        $pelanggan = Pelanggan::findOrfail($pelanggan_id);
+        $pelanggan    = Pelanggan::findOrfail($pelanggan_id);
 
         $pelanggan->first_name = $request->first_name;
-        $pelanggan->last_name = $request->last_name;
-        $pelanggan->birthday = $request->birthday;
-        $pelanggan->gender = $request->gender;
-        $pelanggan->email = $request->email;
-        $pelanggan->phone = $request->phone;
+        $pelanggan->last_name  = $request->last_name;
+        $pelanggan->birthday   = $request->birthday;
+        $pelanggan->gender     = $request->gender;
+        $pelanggan->email      = $request->email;
+        $pelanggan->phone      = $request->phone;
 
         $pelanggan->save();
         return redirect()->route('pelanggan.index')->with('update', 'Data Berhasil Diupdate!');
